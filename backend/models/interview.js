3b4c0509c6c1beeb0dd0cac570327435356d0db4
Urooj Tahir <uroojt113@gmail.com>
2021-03-10 09:41:47 +0500

    View Interview Backend

--- backend/models/interview.js
+++ backend/models/interview.js
@@ -6,7 +6,11 @@ const interviewSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: String, required: true },
+<<<<<<< Updated upstream
   time: { type: String, required: true},
+=======
+ 
+>>>>>>> Stashed changes
   isCancelled:{type:Boolean,default:false},
   field:{type:mongoose.Types.ObjectId,required:true,ref:"Field"},
   creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
